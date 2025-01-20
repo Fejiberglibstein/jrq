59b4eccf1ff3b97758ded611fc23c3579d294b39
Austin Thein <amthein321@gmail.com>
2025-01-20 17:40:39 -0500

    More testing (it all works (probably]))

--- test_both.c
+++ test_both.c
@@ -6,10 +6,11 @@
 
 #define FLAGS JSON_NO_COMPACT | JSON_COLOR
 #define string(v) v, sizeof(v)
+#define all(v) v, v, sizeof(v)
 
-void test(char *initial_str, int str_len) {
-
-    Json *json = json_deserialize(initial_str);
+void test(char *input, char *expected, int str_len) {
+    printf("%s\n", input);
+    Json *json = json_deserialize(input);
     if (str_len == 0) {
         assert(json == NULL);
         free(json);
@@ -17,39 +18,66 @@ void test(char *initial_str, int str_len) {
     }
 
     char *ser = json_serialize(json, 0);
-    if (strncmp(ser, initial_str, str_len) != 0) {
-        printf("`%s` != `%s`\n", ser, initial_str);
-        assert(ser == initial_str && "Strings didn't match");
+    if (strncmp(ser, expected, str_len) != 0) {
+        printf("`%s` != `%s`\n", ser, expected);
+        assert(ser == expected && "Strings didn't match");
     }
 
     free(ser);
     free(json);
+    // printf("hiii\n");
 }
 
 void test_simple() {
-    test(string("10"));
-    test(string("true"));
-    test(string("\"foo \""));
-    test(string("10.200000"));
-    test(string("null"));
+    test("   10", string("10"));
+    test("true    ", string("true"));
+    test("\"foo \"   ", string("\"foo \""));
+    test("   10.200000   ", string("10.200000"));
+    test("   -492   ", string("-492"));
+    test("null  ", string("null"));
 
-    test("10n", 0);
-    test("truen", 0);
-    test("bleh", 0);
-    test("\"fooo\\\"", 0);
+    test("10n", NULL, 0);
+    test("10 0", NULL, 0);
+    test("true n", NULL, 0);
+    test("bleh", NULL, 0);
+    test("\"fooo\\\"", NULL, 0);
+    test("\"fooo\"     h", NULL, 0);
 }
 
 void test_list() {
-    test(string("[]"));
-    test(string("[10]"));
-    test(string("[10, 4]"));
-    test(string("[10, 4, \"help me\", \"die\"]"));
-    test(string("[true, [true, false]]"));
-    test(string("[true, [true], [true, false]]"));
+    test("  []    ", string("[]"));
+    test("  [10]   ", string("[10]"));
+    test("[   10,4  ]  ", string("[10, 4]"));
+    test(" [    true   ,[  10 ]] ", string("[true, [10]]"));
+    test(all("[10, 4, \"help me\", \"die\"]"));
+    test(all("[true, []]"));
+    test(all("[true, [true, false]]"));
+    test(all("[true, false, [true], false]"));
+    test(all("[true, false, [1], [true, false]]"));
+    test(all("[true, [1, [2, 3], 4, 5], [true, false]]"));
+    test(all("[true, [1, [2, 3], 4, [-3, 2], 4], [true, false]]"));
+}
+
+void test_struct() {
+    test("   {} ", string("{}"));
+    test("{   \" hhhi\"  :  10  }", string("{\" hhhi\": 10}"));
+    test("{   \" hhhi\"  :  10 ,\"10\" :null ,}", string("{\" hhhi\": 10, \"10\": null}"));
+    test(all("{\"foo\": 10, \"fooo\": {}}"));
+    test(all("{\"foo\": 10, \"bleh\": {\"in\": null, \"out\": {\"sh\": false}, \"hi\": null, "
+             "\"hi2\": {}, \"j\": 3}, \"h\": -1}"));
+}
+
+void test_list_and_struct() {
+    test(all("[10, {}, [], {\"nehg\": null}]"));
+    test(all("{\"foo\": {}, \"grg\": -20, \"bh\": [10, 9, 2, [3, 5, {\"hi\": [4]}, 8, 3], 2, 8]}"));
 }
 
 int main() {
     test_simple();
     printf("\n");
     test_list();
+    printf("\n");
+    test_struct();
+    printf("\n");
+    test_list_and_struct();
 }
