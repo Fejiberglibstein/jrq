bbdf0defd25de1aaa1200a389fe3018933f3e61a
Austin Thein <amthein321@gmail.com>
2025-01-31 09:30:12 -0500

    Basic token parsing

--- src/lexer.c
+++ src/lexer.c
@@ -1,5 +1,7 @@
 #include "./lexer.h"
 #include "utils.h"
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 
 typedef struct {
@@ -27,18 +29,87 @@ char *next_char(Lexer *l) {
     return l->str++;
 }
 
+char *peek_char(Lexer *l) {
+    return l->str + 1;
+}
+
 void skip_whitespace(Lexer *l) {
     while (is_whitespace(*l->str)) {
         next_char(l);
     }
 }
 
+bool is_digit(char c) {
+    return '0' <= c && c <= '9';
+}
+
+bool is_alpha(char c) {
+    return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
+}
+
 ParseResult parse_ident(Lexer *l) {
     char *start = l->str;
+    Position start_position = l->position;
+
+    char c = *peek_char(l);
+    while (is_alpha(c) || is_digit(c)) {
+        next_char(l);
+        c = *peek_char(l);
+    }
+
+    uint size = (uint)(l->str - start) + 1;
+
+    char *ident = malloc(size + 1);
+    assert_ptr(ident);
+    strncpy(ident, start, size);
+    ident[size] = '\0';
+
+    return (ParseResult) {
+        .token = (Token) {
+            .type = TOKEN_IDENT, 
+            .inner.ident = ident,
+            .range = (Range) {
+                .start = start_position,
+                .end = l->position,
+            }
+        },
+    };
 }
 
 ParseResult parse_string(Lexer *l) {
     char *start = l->str;
+    Position start_position = l->position;
+
+    bool backslashed = false;
+
+    char c = *peek_char(l);
+    while (c != '"' && !backslashed) {
+        backslashed = false;
+        if (c == '\\') {
+            backslashed = true;
+        }
+
+        next_char(l);
+        c = *peek_char(l);
+    }
+
+    uint size = (uint)(l->str - start) + 1;
+
+    char *ident = malloc(size + 1);
+    assert_ptr(ident);
+    strncpy(ident, start, size);
+    ident[size] = '\0';
+
+    return (ParseResult) {
+        .token = (Token) {
+            .type = TOKEN_IDENT, 
+            .inner.ident = ident,
+            .range = (Range) {
+                .start = start_position,
+                .end = l->position,
+            }
+        },
+    };
 }
 
 Token *lex(char *input) {
@@ -68,7 +139,6 @@ Token *lex(char *input) {
             break;
         case '0' ... '9':
             break;
-            
         }
     }
 }
