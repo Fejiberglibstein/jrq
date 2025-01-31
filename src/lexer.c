6b0815180f85f265434a79a39a869c56d905887e
Austin Thein <amthein321@gmail.com>
2025-01-31 00:46:34 -0500

    Get basic lexer structure setup

--- src/lexer.c
+++ src/lexer.c
@@ -1,5 +1,6 @@
 #include "./lexer.h"
 #include "utils.h"
+#include <sys/types.h>
 
 typedef struct {
     char *str;
@@ -7,18 +8,37 @@ typedef struct {
 } Lexer;
 
 typedef struct {
-
+    Token *data;
+    uint length;
+    uint capacity;
 } TokenBuf;
+
+typedef struct {
+    Token token;
+    char *error_message;
+} ParseResult;
+
+char *next_char(Lexer *l) {
+    if (*l->str == '\n') {
+        l->position.line += 1;
+        l->position.col = 0;
+    }
+    l->position.col++;
+    return l->str++;
+}
 
 void skip_whitespace(Lexer *l) {
     while (is_whitespace(*l->str)) {
-        if (*l->str == '\n') {
-            l->position.line += 1;
-            l->position.col = 0;
-        }
-        l->str++;
-        l->position.col += 1;
+        next_char(l);
     }
+}
+
+ParseResult parse_ident(Lexer *l) {
+    char *start = l->str;
+}
+
+ParseResult parse_string(Lexer *l) {
+    char *start = l->str;
 }
 
 Token *lex(char *input) {
@@ -29,4 +49,26 @@ Token *lex(char *input) {
         },
         .str = input,
     };
+    const int INITIAL_CAPACITY = sizeof(Token) * 4;
+    TokenBuf b = (TokenBuf) {
+        .data = malloc(INITIAL_CAPACITY),
+        .length = 0,
+        .capacity = INITIAL_CAPACITY,
+    };
+
+    for (;;) {
+        switch (*l.str) {
+        case 'a' ... 'z':
+        case 'A' ... 'Z':
+        case '_':
+            parse_ident(&l);
+            break;
+        case '"':
+            parse_string(&l);
+            break;
+        case '0' ... '9':
+            break;
+            
+        }
+    }
 }
