9388f8ab2637b4f1265d4e74e8f5d8ef6331655c
Austin Thein <amthein321@gmail.com>
2025-02-02 11:59:17 -0500

    macro for binary expressions

--- src/parser.c
+++ src/parser.c
@@ -16,6 +16,17 @@ typedef struct {
 // clang-format on
 #define LIST(v...) (v), (sizeof(v) / sizeof(*v))
 
+static ASTNode *binary_factor(Parser *p);
+static ASTNode *binary_term(Parser *p);
+static ASTNode *binary_comparison(Parser *p);
+static ASTNode *binary_equality(Parser *p);
+static ASTNode *binary_logical_and(Parser *p);
+static ASTNode *binary_logical_or(Parser *p);
+
+static ASTNode *expression(Parser *p);
+static ASTNode *unary(Parser *p);
+static ASTNode *primary(Parser *p);
+
 static void next(Parser *p) {
     LexResult t = lex_next_tok(p->l);
     if (t.error_message != NULL) {
@@ -38,25 +49,39 @@ static bool matches(Parser *p, TokenType types[], int length) {
     return false;
 }
 
-static ASTNode *expression(Parser *p) {
-    ASTNode *expr = logical_or(p);
-
-    while (matches(p, LIST((TokenType[]) {TOKEN_OR}))) {
-        TokenType operator= p->prev.type;
-
-        ASTNode *rhs = logical_or(p);
-        ASTNode *new_expr = calloc(sizeof(ASTNode), 1);
-        assert_ptr(new_expr);
-
-        new_expr->type = AST_TYPE_BINARY;
-        new_expr->inner.binary = (typeof (new_expr->inner.binary)){
-            .lhs = expr,
-            .operator = operator,
-            .rhs = rhs
-        };
-        expr = new_expr;
+#define PARSE_BINARY_OP(_name_, _next_, _ops_...)                                                  \
+    static ASTNode *_name_(Parser *p) {                                                            \
+        ASTNode *expr = _next_(p);                                                                 \
+                                                                                                   \
+        while (matches(p, LIST((TokenType[])_ops_))) {                                             \
+            TokenType operator= p->prev.type;                                                      \
+                                                                                                   \
+            ASTNode *rhs = _next_(p);                                                              \
+            ASTNode *new_expr = calloc(sizeof(ASTNode), 1);                                        \
+            assert_ptr(new_expr);                                                                  \
+                                                                                                   \
+            new_expr->type = AST_TYPE_BINARY;                                                      \
+            new_expr->inner.binary = (typeof(new_expr->inner.binary)) {                            \
+                .lhs = expr,                                                                       \
+                .operator= operator,                                                               \
+                .rhs = rhs,                                                                        \
+            };                                                                                     \
+            expr = new_expr;                                                                       \
+        }                                                                                          \
+        return expr;                                                                               \
     }
-    return expr;
+
+// clang-format off
+PARSE_BINARY_OP(binary_factor, unary, {TOKEN_SLASH, TOKEN_ASTERISK});
+PARSE_BINARY_OP(binary_term, binary_factor, {TOKEN_PLUS, TOKEN_DASH});
+PARSE_BINARY_OP(binary_comparison, binary_term, {TOKEN_LT_EQUAL, TOKEN_LANGLE, TOKEN_GT_EQUAL, TOKEN_RANGLE});
+PARSE_BINARY_OP(binary_equality, binary_comparison, {TOKEN_EQUAL, TOKEN_NOT_EQUAL});
+PARSE_BINARY_OP(binary_logical_and, binary_equality, {TOKEN_AND});
+PARSE_BINARY_OP(binary_logical_or, binary_logical_and, {TOKEN_OR});
+// clang-format on
+
+static ASTNode *expression(Parser *p) {
+    return binary_logical_or(p);
 }
 
 ParseResult ast_parse(Lexer *l) {
