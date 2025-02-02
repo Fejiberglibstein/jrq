720a48f5f9ccafc1192e101ad8bf15a7720e3640
Austin Thein <amthein321@gmail.com>
2025-02-02 14:00:23 -0500

    Finish primary and unary parsers

--- src/parser.c
+++ src/parser.c
@@ -1,6 +1,8 @@
 #include "src/parser.h"
+#include "src/errors.h"
 #include "src/lexer.h"
 #include "src/utils.h"
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -49,6 +51,14 @@ static bool matches(Parser *p, TokenType types[], int length) {
     return false;
 }
 
+static void expect(Parser *p, TokenType expected, char *err) {
+    if (p->curr.type == expected) {
+        next(p);
+        return;
+    }
+    p->error = err;
+}
+
 #define PARSE_BINARY_OP(_name_, _next_, _ops_...)                                                  \
     static ASTNode *_name_(Parser *p) {                                                            \
         ASTNode *expr = _next_(p);                                                                 \
@@ -82,6 +92,62 @@ PARSE_BINARY_OP(binary_logical_or, binary_logical_and, {TOKEN_OR});
 
 static ASTNode *expression(Parser *p) {
     return binary_logical_or(p);
+}
+
+static ASTNode *unary(Parser *p) {
+    if (matches(p, LIST((TokenType[]) {TOKEN_MINUS, TOKEN_BANG}))) {
+        TokenType operator= p->prev.type;
+        ASTNode *rhs = unary(p);
+
+        ASTNode *new_expr = calloc(sizeof(ASTNode), 1);
+        assert_ptr(new_expr);
+
+        new_expr->type = AST_TYPE_UNARY;
+        new_expr->inner.unary = (typeof(new_expr->inner.unary)) {.rhs = rhs, .operator= operator, };
+        return new_expr;
+    }
+    return primary(p);
+}
+
+static ASTNode *keyword(Parser *p, ASTNodeType t) {
+    ASTNode *n = calloc(sizeof(ASTNode), 1);
+    assert_ptr(n);
+
+    n->type = t;
+    return n;
+}
+
+static ASTNode *primary(Parser *p) {
+    // clang-format off
+    if (matches(p, LIST((TokenType[]) {TOKEN_TRUE}))) return keyword(p, AST_TYPE_TRUE);
+    if (matches(p, LIST((TokenType[]) {TOKEN_FALSE}))) return keyword(p, AST_TYPE_FALSE);
+    if (matches(p, LIST((TokenType[]) {TOKEN_NULL}))) return keyword(p, AST_TYPE_NULL);
+    // clang-format on
+
+    if (matches(p, LIST((TokenType[]) {TOKEN_STRING, TOKEN_INT, TOKEN_DOUBLE, TOKEN_IDENT}))) {
+        ASTNode *new_expr = calloc(sizeof(ASTNode), 1);
+        assert_ptr(new_expr);
+
+        new_expr->type = AST_TYPE_PRIMARY;
+        new_expr->inner.primary = p->prev;
+        return new_expr;
+    }
+
+    if (matches(p, LIST((TokenType[]) {TOKEN_LPAREN}))) {
+        ASTNode *expr = expression(p);
+        expect(p, TOKEN_RPAREN, ERROR_MISSING_RPAREN);
+
+        ASTNode *grouping = calloc(sizeof(ASTNode), 1);
+        assert_ptr(grouping);
+
+        grouping->type = AST_TYPE_GROUPING;
+        grouping->inner.grouping = expr;
+    }
+
+    // todo
+    p->error = ERROR_UNEXPECTED_TOKEN;
+
+    return NULL;
 }
 
 ParseResult ast_parse(Lexer *l) {
