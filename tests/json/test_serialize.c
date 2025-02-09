7f2b4b18ec5b9228cb8c80fb6f0786207e8c55a2
Austin Thein <amthein321@gmail.com>
2025-02-09 13:23:21 -0500

    Testing lists

--- tests/json/test_serialize.c
+++ tests/json/test_serialize.c
@@ -4,10 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-void test(char *expected, Json *input) {
+#define DEFAULT_FLAGS JSON_FLAG_SPACES
+
+void test(char *expected, Json *input, JsonSerializeFlags flags) {
     printf("testing %s\n", expected);
 
-    JsonSerializeFlags flags = JSON_FLAG_SPACES;
     char *res = json_serialize(input, flags);
 
     if (strcmp(res, expected) != 0) {
@@ -19,11 +20,64 @@ void test(char *expected, Json *input) {
 }
 
 void test_primitives() {
-    test("10", &(Json) {.type = JSON_TYPE_NUMBER, .inner.number = 10});
-    test("true", &(Json) {.type = JSON_TYPE_BOOL, .inner.boolean = true});
-    test("\"fooo\"", &(Json) {.type = JSON_TYPE_STRING, .inner.string = "fooo"});
+    test("10", &(Json) {.type = JSON_TYPE_NUMBER, .inner.number = 10}, DEFAULT_FLAGS);
+    test("-29.731", &(Json) {.type = JSON_TYPE_NUMBER, .inner.number = -29.731}, DEFAULT_FLAGS);
+    test("true", &(Json) {.type = JSON_TYPE_BOOL, .inner.boolean = true}, DEFAULT_FLAGS);
+    test("false", &(Json) {.type = JSON_TYPE_BOOL, .inner.boolean = false}, DEFAULT_FLAGS);
+    test("\"fooo\"", &(Json) {.type = JSON_TYPE_STRING, .inner.string = "fooo"}, DEFAULT_FLAGS);
+    test("null", &(Json) {.type = JSON_TYPE_NULL}, DEFAULT_FLAGS);
+}
+
+void test_list() {
+    test("[]", &(Json) {.type = JSON_TYPE_LIST, .inner.list = {0}}, DEFAULT_FLAGS);
+
+    test(
+        "[true]",
+        &(Json) {
+            .type = JSON_TYPE_LIST,
+            .inner.list = (JsonIterator){
+                .data = (Json[]) {
+                    (Json) {.type = JSON_TYPE_BOOL, .inner.boolean = true},
+                },
+                .length = 1,
+            },
+        },
+        DEFAULT_FLAGS
+    );
+
+    test(
+        "[10.2]",
+        &(Json) {
+            .type = JSON_TYPE_LIST,
+            .inner.list = (JsonIterator){
+                .data = (Json[]) {
+                    (Json) {.type = JSON_TYPE_NUMBER, .inner.number = 10.2},
+                },
+                .length = 1,
+            },
+        },
+        DEFAULT_FLAGS
+    );
+
+    test(
+        "[true, false, 10.2, \"blehg\"]",
+        &(Json) {
+            .type = JSON_TYPE_LIST,
+            .inner.list = (JsonIterator){
+                .data = (Json[]) {
+                    (Json) {.type = JSON_TYPE_BOOL, .inner.boolean = true},
+                    (Json) {.type = JSON_TYPE_BOOL, .inner.boolean = false},
+                    (Json) {.type = JSON_TYPE_NUMBER, .inner.number = 10.2},
+                    (Json) {.type = JSON_TYPE_STRING, .inner.string = "blehg"},
+                },
+                .length = 4,
+            },
+        },
+        DEFAULT_FLAGS
+    );
 }
 
 int main() {
     test_primitives();
+    test_list();
 }
