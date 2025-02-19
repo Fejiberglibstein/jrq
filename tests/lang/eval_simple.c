cf4cf0b799f1e0a73e715017f1cf5687fa0fe5f5
Austin Thein <amthein321@gmail.com>
2025-02-19 11:34:36 -0500

    Eval testing

--- tests/lang/eval_simple.c
+++ tests/lang/eval_simple.c
@@ -36,7 +36,17 @@ bool test_eval(char *expr, Json input, Json expected) {
 
 void simple_eval() {
     assert(test_eval("", json_number(10), json_number(10)));
-    assert(test_eval("10 + 10", json_null(), json_number(20)));
+    assert(test_eval("10 + 102", json_null(), json_number(112)));
+    assert(test_eval("10 + 10 * 2", json_null(), json_number(30)));
+    assert(test_eval("12 / 3 - 3", json_null(), json_number(1)));
+    assert(test_eval("12 % 3", json_null(), json_number(0)));
+    assert(test_eval("(12 - 3) * 4", json_null(), json_number(9 * 4)));
+    assert(test_eval("true != false", json_null(), json_boolean(true)));
+    assert(test_eval("12 >= 3", json_null(), json_boolean(true)));
+    assert(test_eval("4-6/2 <= (4-6)/2", json_null(), json_boolean(4 - 6 / 2 <= (4 - 6) / 2)));
+    assert(test_eval("\"blehh\" == \"blehh\"", json_null(), json_boolean(true)));
+    assert(test_eval("\"blehh\" != \"stupid\"", json_null(), json_boolean(true)));
+    // assert(test_eval("{\"foo\": 10} == {\"foo\": 10-2}", json_null(), json_boolean(true)));
 }
 
 int main() {
