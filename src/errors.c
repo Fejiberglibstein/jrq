4a47ccdf51caa0bb43e6068c27dbb2de52ae2b83
Austin Thein <amthein321@gmail.com>
2025-03-26 23:45:47 -0400

    Make use of the refactor

--- src/errors.c
+++ src/errors.c
@@ -38,9 +38,6 @@ struct error_msg_data {
 };
 
 struct error_msg_data jrq_get_error_data(JrqError err, char *start) {
-    char *err_start = start;
-    char *err_end = start;
-
     // First character of the line for the range start's line
     char *start_line = start;
     // First character of the line for the range end's line
@@ -91,65 +88,39 @@ struct error_msg_data jrq_get_error_data(JrqError err, char *start) {
 
     const int MARGIN = 5;
 
+    char *err_end = end_line + err.range.end.col;
+    char *err_start = start_line + err.range.start.col;
+
     return (struct error_msg_data) {
         .err_end = err_end,
         .err_start = err_start,
         .height = err.range.end.line - err.range.start.line + 1,
         .width = highest_width,
 
-        .margin_start = MAX(start - MARGIN - 1, start_line),
-        .margin_end = MIN(end_line + MARGIN - 1, end),
+        .margin_start = MAX(err_start - MARGIN - 1, start_line),
+        .margin_end = MIN(err_end + MARGIN - 1, end),
 
     };
 }
 
 char *jrq_error_format(JrqError err, char *text) {
 
-    char *start_line = text;
-    char *end_line = text;
-    char *text_end = text;
+    struct error_msg_data data = jrq_get_error_data(err, text);
 
-    int new_lines = 1;
-    // move start_line and end_line to the correct line.
-    for (text_end = text; new_lines <= err.range.end.line && *text_end != '\0'; text_end++) {
-        if (new_lines < err.range.start.line) {
-            start_line++;
-        }
-        if (new_lines < err.range.end.line) {
-            end_line++;
-        }
-        if (*text_end == '\n') {
-            new_lines++;
-        }
-    }
-    // Go past the new lines
-    if (*start_line == '\n') {
-        start_line++;
-    }
-    end_line++;
-    text_end--;
-
-    char *start = start_line + err.range.start.col;
-    char *end = end_line + err.range.end.col;
-
-    const int MARGIN = 5;
-
-    char *start_margin = MAX(start - MARGIN - 1, start_line);
-    char *end_margin = MIN(end + MARGIN - 1, text_end);
-
-    size_t err_len = end - start;
+    size_t err_len = data.err_end - data.err_start;
     char *arrow_txt = malloc(err_len + 1);
     memset(arrow_txt, '~', err_len);
     arrow_txt[0] = '^';
     arrow_txt[err_len] = '\0';
 
-    uint margin_len = end_margin - start_margin;
+    uint margin_len = data.margin_end - data.margin_start;
 
 #define ERROR                                                                                      \
     "%.*s\n" /* printing the code that has the error */                                            \
     "%*s\n"  /* printing the arrows pointing to what caused the error */                           \
     "%s\n",  /* printing the error message */                                                      \
-        margin_len, start_margin, (int)(start - start_margin + err_len - 1), arrow_txt, err.err
+        margin_len, data.margin_start, (int)(data.err_start - data.margin_start + err_len - 1),    \
+        arrow_txt, err.err
 
     int length = snprintf(NULL, 0, ERROR);
     char *v = malloc(length);
